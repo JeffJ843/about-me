d0c01b5b231b2837169672aa723861fed38ff197
Jeffery Johnson <Deangelojohnson75@gmail.com>
2022-03-08 17:15:09 -0600

    updated files

---
+++ js/app.js
@@ -0,0 +1,52 @@
+'use strict'
+
+let answerone = prompt('Hey! what is your name?').toUpperCase();
+
+if (answerone === 'Y' || answerone === 'YES'){
+  alert('you are correct!');
+}else if(answerone === 'N' || answerone === 'NO') {
+  alert('sorry, you are incorrect')
+}else{
+  alert('Oh ok')
+}
+
+let answertwo = prompt('Do You see?').toUpperCase();
+
+if (answertwo === 'Y' || answertwo === 'YES'){
+  alert('you are correct!');
+}else if(answerone === 'N' || answerone === 'NO') {
+  alert('sorry, you are incorrect')
+}else{
+  alert('did not understand')
+}
+
+let answerthree = prompt('Are you human by chance').toUpperCase();
+
+if (answerthree === 'Y' || answerthree === 'YES'){
+  alert('you are correct!' + username);
+}else if(answerthree === 'N' || answerthree === 'NO') {
+  alert('sorry, you are incorrect')
+}else{
+  alert('did not understand')
+}
+
+let answerfour= prompt('Do You see?').toUpperCase();
+
+if (answerfour === 'Y' || answerfour === 'YES'){
+  alert('you are correct!' + username);
+}else if(answerfour === 'N' || answerfour === 'NO') {
+  alert('sorry, you are incorrect')
+}else{
+  alert('did not understand')
+}
+
+let answerfive = prompt('Had fun?' ).toUpperCase();
+
+if (answerfive === 'Y' || answerfive === 'YES'){
+  alert('Good for you!' + username);
+}else if(answerfive === 'N' || answerfive === 'NO') {
+  alert('how rude' + username)
+}else{
+  alert('did not understand' + username) 
+}
+
